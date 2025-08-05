4dea03521b52132cc4a01540a93133fa1b33bb07
Polina Cherkasova <polina.c@live.com>
2025-08-04 19:50:46 -0700

    Make scaffold looking closer to travel-app mocks (#69)

--- examples/travel_app/lib/main.dart
+++ examples/travel_app/lib/main.dart
@@ -75,6 +75,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dynamic UI Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
@@ -125,8 +126,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Dynamic UI Demo'),
+        leading: const Icon(Icons.menu),
+        title: const Row(
+          children: <Widget>[
+            Icon(Icons.chat_bubble_outline),
+            SizedBox(width: 8.0), // Add spacing between icon and text
+            Text('Dynamic UI Demo'),
+          ],
+        ),
+        actions: [const Icon(Icons.person_outline), const SizedBox(width: 8.0)],
       ),
       body: Center(
         child: ConstrainedBox(
