a81e441a2cfac0a55a2264aca04e218ec7345542
Greg Spencer <gspencergoog@users.noreply.github.com>
2025-08-05 17:52:34 -0700

    fix some analyzer infos (#81)
    * fix some analyzer infos
    
    * Fix CI

--- pkgs/spikes/usage_test/lib/main.dart
+++ pkgs/spikes/usage_test/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_genui/flutter_genui.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
