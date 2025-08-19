a56b8a8a15878c3df476aa73875ee1944e19a4f2
jacobsimionato <jsimionato@google.com>
2025-08-19 12:25:13 +0930

    Rename .images.json to _images.json to fix deployed web app (#138)

--- examples/travel_app/lib/src/asset_images.dart
+++ examples/travel_app/lib/src/asset_images.dart
@@ -8,7 +8,7 @@ import 'package:flutter/widgets.dart';
 @visibleForTesting
 const assetImageCatalogPath = 'assets/travel_images';
 @visibleForTesting
-const assetImageCatalogJsonFile = '$assetImageCatalogPath/.images.json';
+const assetImageCatalogJsonFile = '$assetImageCatalogPath/_images.json';
 
 /// Loads the asset image catalog from the asset bundle and prepends the asset
 /// path to the image file names.
