c52538f345725b0206db09d1ff4f8a1b13d5d472
Polina Cherkasova <polina.c@live.com>
2025-08-20 10:23:26 -0700

    Create fix_copyright.sh (#161)

---
+++ tool/fix_copyright.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright 2025 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Fixes copyright headers to make bots happy.
+
+# Fast fail the script on failures.
+set -ex
+
+# The directory that this script is located in.
+TOOL_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+
+# The year is hardcoded to 2025, year of project creation.
+dart "$TOOL_DIR/fix_copyright/bin/fix_copyright.dart" --year 2025 --force "$@"
