25c6da2d0b99e904f6b50e0cf72dffea7e07b105
Greg Spencer <gspencergoog@users.noreply.github.com>
2025-08-25 12:21:47 -0700

    Adding modified gradle files (#190)

--- examples/simple_chat/android/app/build.gradle.kts
+++ examples/simple_chat/android/app/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
 }
 
 android {
