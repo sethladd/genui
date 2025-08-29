9e81b0ac7802b3a6c6b53525b0a0f2dce55ba1c5
Polina Cherkasova <polina.c@live.com>
2025-08-28 18:24:32 -0700

    Move gallery to packages and delete minimal_genui. (#220)

--- tool/stub_firebase_options.sh
+++ tool/stub_firebase_options.sh
@@ -8,6 +8,5 @@
 # Fast fail the script on failures.
 set -ex
 
-cp -f examples/minimal_genui/lib/firebase_options_stub.dart examples/minimal_genui/lib/firebase_options.dart
 cp -f examples/travel_app/lib/firebase_options_stub.dart examples/travel_app/lib/firebase_options.dart
 cp -f examples/simple_chat/lib/firebase_options_stub.dart examples/simple_chat/lib/firebase_options.dart
