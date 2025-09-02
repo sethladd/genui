1fca0b077ffb13f1ed635e822910c4bcd011201a
jacobsimionato <jsimionato@google.com>
2025-09-03 07:46:38 +0930

    Move genui_surface.dart to core and rename test (#263)

--- packages/flutter_genui/lib/flutter_genui.dart
+++ packages/flutter_genui/lib/flutter_genui.dart
@@ -13,9 +13,8 @@ export 'src/catalog/core_widgets/text.dart';
 export 'src/catalog/core_widgets/text_field.dart';
 export 'src/core/core_catalog.dart';
 export 'src/core/genui_manager.dart';
+export 'src/core/genui_surface.dart';
 export 'src/core/widgets/chat_primitives.dart';
-export 'src/facade/genui_surface.dart';
-
 export 'src/facade/ui_agent.dart';
 export 'src/model/catalog.dart';
 export 'src/model/catalog_item.dart';
