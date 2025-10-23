aaeb982b99d67be19ebe6c6d239ec7d7632fcf69
jacobsimionato <jsimionato@google.com>
2025-10-23 13:15:05 +1100

    Move test utils to be exported from the flutter_genui library (#434)

--- examples/travel_app/test/catalog_validation_test.dart
+++ examples/travel_app/test/catalog_validation_test.dart
@@ -2,11 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_genui/src/catalog/core_catalog.dart';
+import 'package:flutter_genui/flutter_genui.dart';
+import 'package:flutter_genui/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:travel_app/src/catalog.dart';
 
-import '../../../packages/flutter_genui/test/validation_test_utils.dart';
-
 void main() {
-  validateCatalogExamples(travelAppCatalog, [CoreCatalogItems.asCatalog()]);
+  group('Travel App Catalog Validation', () {
+    final mergedCatalog = Catalog([
+      ...travelAppCatalog.items,
+      ...CoreCatalogItems.asCatalog().items,
+    ]);
+
+    for (final item in travelAppCatalog.items) {
+      test('CatalogItem ${item.name} examples are valid', () async {
+        final errors = await validateCatalogItemExamples(item, mergedCatalog);
+        expect(errors, isEmpty, reason: errors.join('\n'));
+      });
+    }
+  });
 }
--- packages/flutter_genui/test/core_catalog_validation_test.dart
+++ packages/flutter_genui/test/core_catalog_validation_test.dart
@@ -2,10 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_genui/src/catalog/core_catalog.dart';
-
-import 'validation_test_utils.dart';
+import 'package:flutter_genui/flutter_genui.dart';
+import 'package:flutter_genui/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  validateCatalogExamples(CoreCatalogItems.asCatalog());
+  group('Core Catalog Validation', () {
+    final mergedCatalog = CoreCatalogItems.asCatalog();
+
+    for (final item in mergedCatalog.items) {
+      test('CatalogItem ${item.name} examples are valid', () async {
+        final errors = await validateCatalogItemExamples(item, mergedCatalog);
+        expect(errors, isEmpty, reason: errors.join('\n'));
+      });
+    }
+  });
 }
